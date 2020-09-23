c5ba3c9c78f8f1ddef0d64c310ac41df234235b8
Makini Bryan <bmakini@ymail.com>
2020-09-23 20:11:18 +0300

    Added the diani campaign package

---
+++ src/HartWeb/ClientApp/src/app/modules/campaigns/components/all-campaigns/index.ts
@@ -0,0 +1,2 @@
+
+export * from './all-campaigns.component';
---
+++ src/HartWeb/ClientApp/src/app/modules/campaigns/components/three-days-diani-x-wasini-island-excursion/three-days-diani-x-wasini-island-excursion.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-three-days-diani-x-wasini-island-excursion',
+  templateUrl: './three-days-diani-x-wasini-island-excursion.component.html',
+  styleUrls: ['./three-days-diani-x-wasini-island-excursion.component.css']
+})
+export class ThreeDaysDianiXWasiniIslandExcursionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

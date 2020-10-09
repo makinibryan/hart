76b8f82eb860c7de8ea3e1a1d56dc6ad114ac0c5
George Odongo <george.odongo@jameshay.co.uk>
2020-10-09 10:39:41 +0100

    Solved merge conflicts

--- src/HartWeb/ClientApp/src/app/modules/campaigns/campaigns.module.ts
+++ src/HartWeb/ClientApp/src/app/modules/campaigns/campaigns.module.ts
@@ -4,7 +4,6 @@ import { SharedModule } from '../../../app/shared/shared.module';
 
 import { CampaignsRoutingModule } from './campaigns-routing.module';
 
-<<<<<<< HEAD
 import {
   AllCampaignsComponent,
   LongonotComponent,
@@ -12,14 +11,6 @@ import {
   MombasaThreeDaysTwoNightsBeachDazeComponent,
   CampaignMaasaiMaraThreeDaysComponent
 } from './components';
-=======
-import { AllCampaignsComponent,
-         LongonotComponent,
-         ThreeDaysDianiXWasiniIslandExcursionComponent,
-         MombasaThreeDaysTwoNightsBeachDazeComponent,
-         CampaignMaasaiMaraThreeDaysComponent
-         } from './components';
->>>>>>> b0c551b98329e433ed47773fff2095098c975115
 
 
 @NgModule({

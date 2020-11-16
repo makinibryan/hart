cba0791c1f47ec4b72719668636c278ba5f81b75
Makini Bryan <bmakini@ymail.com>
2020-11-16 11:03:36 +0300

    added the Naivasha campign to the staging area

--- src/HartWeb/ClientApp/src/app/modules/campaigns/campaigns-routing.module.ts
+++ src/HartWeb/ClientApp/src/app/modules/campaigns/campaigns-routing.module.ts
@@ -1,59 +1,60 @@
 import { Component, NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-import { AllCampaignsComponent,
-        LongonotComponent,
-        MombasaThreeDaysTwoNightsBeachDazeComponent,
-        ThreeDaysDianiXWasiniIslandExcursionComponent,
-        CampaignMaasaiMaraThreeDaysComponent,
-        CampaignZipLiningAndTeamBuildingNgongHillsComponent,
-        CampaignSamburuOverlandTruckGameDriveComponent,
-        CampaignOlooluaComponent,
-        CampaignNnpKitengelaComponent,
-        CampaignLakeNaivashaComponent
-      } from './components';
+import {
+  AllCampaignsComponent,
+  LongonotComponent,
+  MombasaThreeDaysTwoNightsBeachDazeComponent,
+  ThreeDaysDianiXWasiniIslandExcursionComponent,
+  CampaignMaasaiMaraThreeDaysComponent,
+  CampaignZipLiningAndTeamBuildingNgongHillsComponent,
+  CampaignSamburuOverlandTruckGameDriveComponent,
+  CampaignOlooluaComponent,
+  CampaignNnpKitengelaComponent,
+  CampaignLakeNaivashaComponent
+} from './components';
 
 const routes: Routes = [
-    {
-      path: '',
-      component: AllCampaignsComponent
-    },
-    {
-      path: 'longonot',
-      component: LongonotComponent
-    },
-    {
-      path: 'diani',
-      component: ThreeDaysDianiXWasiniIslandExcursionComponent
-    },
-    {
-      path: 'mombasa',
-      component: MombasaThreeDaysTwoNightsBeachDazeComponent
-    },
-    {
-      path: 'maasai-mara',
-      component: CampaignMaasaiMaraThreeDaysComponent
-    },
-    {
-      path: 'zip-lining-ngong-hills',
-      component: CampaignZipLiningAndTeamBuildingNgongHillsComponent
-    },
-    {
-      path: 'samburu-overland-truck-game-drive',
-      component: CampaignSamburuOverlandTruckGameDriveComponent
-    },
-    {
-      path: 'oloolua-nature-trails',
-      component: CampaignOlooluaComponent
-    },
-    {
-      path: 'nairobi-national-park-and-kitengela-glass-adventure',
-      component: CampaignNnpKitengelaComponent
-    },
-    {
-      path: 'lake-naivasha-oloiden-olkaria-spa-hells-gate-crater-lake',
-      component: CampaignLakeNaivashaComponent
-    }
+  {
+    path: '',
+    component: AllCampaignsComponent
+  },
+  {
+    path: 'longonot',
+    component: LongonotComponent
+  },
+  {
+    path: 'diani',
+    component: ThreeDaysDianiXWasiniIslandExcursionComponent
+  },
+  {
+    path: 'mombasa',
+    component: MombasaThreeDaysTwoNightsBeachDazeComponent
+  },
+  {
+    path: 'maasai-mara',
+    component: CampaignMaasaiMaraThreeDaysComponent
+  },
+  {
+    path: 'zip-lining-ngong-hills',
+    component: CampaignZipLiningAndTeamBuildingNgongHillsComponent
+  },
+  {
+    path: 'samburu-overland-truck-game-drive',
+    component: CampaignSamburuOverlandTruckGameDriveComponent
+  },
+  {
+    path: 'oloolua-nature-trails',
+    component: CampaignOlooluaComponent
+  },
+  {
+    path: 'nairobi-national-park-and-kitengela-glass-adventure',
+    component: CampaignNnpKitengelaComponent
+  },
+  {
+    path: 'lake-naivasha-oloiden-olkaria-spa-hells-gate-crater-lake',
+    component: CampaignLakeNaivashaComponent
+  }
 ];
 
 
